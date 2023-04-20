28c9bd04d30b1e742e987a0658fb3ad54d1bb036
刘健 <liujian@liujiandeMacBook-Pro.local>
2023-04-20 15:57:38 +0800

    fix

--- lc_0012/Solution12.cpp
+++ lc_0012/Solution12.cpp
@@ -4,12 +4,26 @@
 
 #include "Solution12.h"
 #include <iostream>
+#include <map>
+#include <sys/time.h>
 
 using namespace std;
 
+std::string GetTime(uint32_t timestamp)
+{
+    char now[64];
+    time_t tt = timestamp;
+    struct tm *ttime = localtime(&tt);
+    strftime(now,64,"%Y-%m-%d %H:%M:%S",ttime);
+    return now;
+}
+
 int main() {
     // todo
-    for (int i = 10; i > 0; i--)
-    cout << Solution().intToRoman(i) << endl;
+//    map<int, std::pair<int , string>> map_;
+//    for (auto it : map_) {
+//        cout << it.first << it.second.first
+//    }
+    cout << GetTime(1672984089) << endl;
     return 0;
 }
